169901b539aac6886c5595d7b7b099163172f7cb
bee <bee85919@gmail.com>
2023-09-10 22:59:04 +0900

    2.4 Maps

---
+++ 2. DataTypes/2.4 Maps2.dart
@@ -0,0 +1,6 @@
+void main() {
+  Map<List<int>, bool> player = {
+    [1,2,3,4]: true,
+  };
+  print(player);
+}
---
+++ 2. DataTypes/2.4 Maps3.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<Map<String, Object>> player = [
+    {
+      'name': 'nico',
+      'xp': 1999.99,
+    },
+    {
+      'name': 'lynn',
+      'xp': 2000.99,
+    }
+  ];
+  print(player);
+}
