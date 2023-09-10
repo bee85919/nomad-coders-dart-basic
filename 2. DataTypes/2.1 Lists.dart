8af22760209d6cf8fd07e8d16f98cb003649dc7e
bee <bee85919@gmail.com>
2023-09-10 22:58:41 +0900

    Create 2.1 Lists.dart

---
+++ 2. DataTypes/2.1 Lists.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool giveMeFive = true;
+  List<int> num1 = [1,2,3,4];
+  var num2 = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+  print(num1);
+  print(num2);
+}
