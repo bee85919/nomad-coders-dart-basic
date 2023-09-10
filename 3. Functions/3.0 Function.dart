7777779cd84eb433868bd2464170009ef677fb6a
bee <bee85919@gmail.com>
2023-09-10 22:59:13 +0900

    Create 3.0 Function.dart

---
+++ 3. Functions/3.0 Function.dart
@@ -0,0 +1,17 @@
+hello(String name) {
+  return "Hello! $name. ";
+}
+
+String say() {
+  return "nice to meet you!";
+}
+
+String hi() { "hi!"; }
+
+String hi2() => "hi!";
+
+num plus(num a, num b) => a + b;
+
+void main() {
+  print(hello("nico"));
+}
